33df815f5e55e6a324c58f370157f91d01fabe9d
c2ber <73525374+c2ber@users.noreply.github.com>
2026-08-08 22:19:35 +0900

    Add rawMenus data and lastUpdate to data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+window.lastUpdate = "2026-08-08";
+
+window.rawMenus = `
+001|한식|삼합 꽃게장|https://naver.me/5UEcbDis|
+002|한식|육회탕탕이|https://naver.me/GHvqMI3S|
+003|한식|선지해장국|https://naver.me/5k7fK6oT|
+004|중식|오리날개튀김|https://naver.me/xxYVpySF|
+005|한식|생선구이|https://naver.me/xLWM2h39|
+006|카페|에그타르트|https://naver.me/5ZJxEgJA|
+007|한식|연포탕|https://naver.me/xY4syoJx|
+008|한식|바지락비빔밥|https://naver.me/GEXGyDw8|
+009|양식|김찌, 함박스테이크|https://naver.me/5WOjh1Ln|
+010|술집|조개찜|https://naver.me/FdCLdRyu|여긴 너무... 술집임
+011|카페|케이블카 타고 카페|https://naver.me/F0z20KWI|
+012|카페|상하목장 아이스크림|https://naver.me/5xjpsOzw|
+
+
+
+`;
