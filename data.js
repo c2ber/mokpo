d7e14efb49a00ca85addec25e59275c58c9134b3
c2ber <73525374+c2ber@users.noreply.github.com>
2026-08-14 15:47:48 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,9 @@
 window.lastUpdate = "2026-08-08";
 
 window.rawMenus = `
-001|한식|독천식당|삼합 꽃게장|https://naver.me/5UEcbDis|꽃게장도 맛있고 삼합도 괜찮음
-002|한식|○○육회|육회탕탕이|https://naver.me/GHvqMI3S|
-003|한식|○○해장국|선지해장국|https://naver.me/5k7fK6oT|해장할 때 좋음
-004|중식|○○반점|오리날개튀김|https://naver.me/xxYVpySF|
-005|한식|○○식당|생선구이|https://naver.me/xLWM2h39|
-006|카페|○○베이커리|에그타르트|https://naver.me/5ZJxEgJA|
-
-
+001|한식|해남해장국|뼈해장국|https://naver.me/Fmf6PDBd|맑은 국물
+002|한식|김정림선지해장국|선지해장|https://naver.me/5k7fK6oT|칼칼함
+003|한식|용당반점|오리날개튀김|https://naver.me/xxYVpySF|맥주안주
 
 
 
