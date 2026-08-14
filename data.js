8d8d3dd6df4b6ba3f6b2e0dd1117e46f578c901c
c2ber <73525374+c2ber@users.noreply.github.com>
2026-08-14 18:20:08 +0900

    Update data.js

--- data.js
+++ data.js
@@ -8,12 +8,17 @@ window.rawMenus = `
 005|밥집|인동주마을|홍어삼합|https://naver.me/5UEcbDis|
 006|해산물|하당먹거리|육회낙지탕탕이|https://naver.me/GHvqMI3S|
 007|해산물|바다포차|계절 해산물,회|https://naver.me/5S9QI0hI|굴보쌈,전어,방어 등등
-008|술집|목포씨|조개전골,조개찜,회|https://naver.me/FdCLdRyu|뉴비..
+008|술집|목포씨|조개전골,조개찜,회|https://naver.me/FdCLdRyu|새로생겼음
 009|해산물|만선식당|벤댕이회|https://naver.me/xOxXHf77|경험 해볼만한 곳
 010|밥집|해빔|꽃게살 해초비빔밥|https://naver.me/xtgpn42x|
 011|밥집|해촌|바지락비빔밥|https://naver.me/5iTdeYDG|
 012|밥집|서울해장국|김치찌개|https://naver.me/5WOjh1Ln|평일 함박스테이크 공짜
 013|해산물|긴자회수산|해산물모듬|https://naver.me/5yPnR8hp|예약필수..
 014|해산물|송학낙지회관|연포탕,호롱이|https://naver.me/xY4syoJx|
+015|밥집|미지아레스토|배추셀러드|https://naver.me/GI3JDJf4|양식임
+016|카페|코롬방제과점|새우바게트|https://naver.me/5YFcB559|
+017|밥집|태동반점|중깐|https://naver.me/F884a3f1|짜장을 시키면 짬뽕이 공짜
+
+
 
 `;
