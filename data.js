b9b3a6780764579ec85bdf9e3436066514f07c03
c2ber <73525374+c2ber@users.noreply.github.com>
2026-08-14 17:43:56 +0900

    Update data.js

--- data.js
+++ data.js
@@ -6,10 +6,13 @@ window.rawMenus = `
 003|기타|용당반점|오리날개튀김|https://naver.me/xxYVpySF|맥주안주
 004|카페|커피창고로|에그타르트|https://naver.me/5ZJxEgJA|10시부터
 005|밥집|인동주마을|홍어삼합|https://naver.me/5UEcbDis|
-006|술집|하당먹거리|육회낙지탕탕이|https://naver.me/GHvqMI3S|
+006|해산물|하당먹거리|육회낙지탕탕이|https://naver.me/GHvqMI3S|
 007|해산물|바다포차|계절 해산물,회|https://naver.me/5S9QI0hI|굴보쌈,전어,방어 등등
 008|술집|목포씨|조개전골,조개찜,회|https://naver.me/FdCLdRyu|뉴비..
 009|해산물|만선식당|벤댕이회|https://naver.me/xOxXHf77|경험 해볼만한 곳
+010|밥집|해촌|바지락비빔밥|https://naver.me/5iTdeYDG|
+011|밥집|해빔|꽃게살 해초비빔밥|https://naver.me/xtgpn42x|
+012|밥집|서울해장국|김치찌개|https://naver.me/5WOjh1Ln|평일 함박스테이크 공짜
 
 
 `;
