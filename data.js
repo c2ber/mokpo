67daee632dd4f855377e2f677bc14d94174af8b8
c2ber <73525374+c2ber@users.noreply.github.com>
2026-08-14 17:14:38 +0900

    Update menu categories and add new entries

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 window.lastUpdate = "2026-08-14";
 
 window.rawMenus = `
-001|한식|해남해장국|뼈해장국|https://naver.me/Fmf6PDBd|맑은 국물
-002|한식|김정림선지해장국|선지해장국|https://naver.me/5k7fK6oT|칼칼함
-003|중식|용당반점|오리날개튀김|https://naver.me/xxYVpySF|맥주안주
+001|밥집|해남해장국|뼈해장국|https://naver.me/Fmf6PDBd|맑은 국물
+002|밥집|김정림선지해장국|선지해장국|https://naver.me/5k7fK6oT|칼칼함
+003|기타|용당반점|오리날개튀김|https://naver.me/xxYVpySF|맥주안주
 004|카페|커피창고로|에그타르트|https://naver.me/5ZJxEgJA|10시부터
-005|한식|인동주마을|홍어삼합|https://naver.me/5UEcbDis|
-006|한식|하당먹거리|육회낙지탕탕이|https://naver.me/GHvqMI3S|
-007|한식|바다포차|계절 해산물,회|https://naver.me/5S9QI0hI|굴보쌈,전어,방어 등등
-008|한식|목포씨|조개전골,조개찜,회|https://naver.me/FdCLdRyu|뉴비..
+005|밥집|인동주마을|홍어삼합|https://naver.me/5UEcbDis|
+006|술집|하당먹거리|육회낙지탕탕이|https://naver.me/GHvqMI3S|
+007|해산물|바다포차|계절 해산물,회|https://naver.me/5S9QI0hI|굴보쌈,전어,방어 등등
+008|술집|목포씨|조개전골,조개찜,회|https://naver.me/FdCLdRyu|뉴비..
 
 `;
