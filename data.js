ca19f0d610c064b99d137e48972fb8c0fa8c8f1a
c2ber <73525374+c2ber@users.noreply.github.com>
2026-08-14 16:02:49 +0900

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,9 @@ window.rawMenus = `
 001|한식|해남해장국|뼈해장국|https://naver.me/Fmf6PDBd|맑은 국물
 002|한식|김정림선지해장국|선지해장|https://naver.me/5k7fK6oT|칼칼함
 003|한식|용당반점|오리날개튀김|https://naver.me/xxYVpySF|맥주안주
-
+004|카페|커피창고로|에그타르트|https://naver.me/5ZJxEgJA|10시부터
+005|한식|인동주마을|홍어삼합|https://naver.me/5UEcbDis|
+006|한식|하당먹거리|육회낙지탕탕이|https://naver.me/GHvqMI3S|
 
 
 `;
