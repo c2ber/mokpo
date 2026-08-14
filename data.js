421830c2ffa575fb6b1b4142da1eddf5f6fe6d64
c2ber <73525374+c2ber@users.noreply.github.com>
2026-08-14 17:33:12 +0900

    Add new seafood restaurant entry to data.js

--- data.js
+++ data.js
@@ -9,5 +9,7 @@ window.rawMenus = `
 006|술집|하당먹거리|육회낙지탕탕이|https://naver.me/GHvqMI3S|
 007|해산물|바다포차|계절 해산물,회|https://naver.me/5S9QI0hI|굴보쌈,전어,방어 등등
 008|술집|목포씨|조개전골,조개찜,회|https://naver.me/FdCLdRyu|뉴비..
+009|해산물|만선식당|벤댕이회|https://naver.me/xOxXHf77|경험 해볼만한 곳
+
 
 `;
