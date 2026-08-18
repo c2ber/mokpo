28d10b7626b2b37221301ef9c3a8e520afdae087
c2ber <73525374+c2ber@users.noreply.github.com>
2026-08-18 09:56:28 +0900

    Update data.js

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ window.rawMenus = `
 018|해산물|선경준치회집|벤댕이회|https://naver.me/F3TX64w8|
 019|카페|SUKSAN|뷰맛집|https://naver.me/5tJTCm58|케이블카 타고 여가가셈
 020|술집|88포장마차 평화광장점|김밥,생똥집|https://naver.me/GqfdupwR|
-
+021|카페|송자르트|소금빵|https://naver.me/5k7TnfzF|
 
 
 `;
